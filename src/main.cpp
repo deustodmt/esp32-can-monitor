99df7af3580c757ed57cf2662b05765982a3ab32
KaRLiToS3 <145749130+KaRLiToS3@users.noreply.github.com>
2025-02-20 18:27:19 +0100

    Lights up a LED

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "WiFi_manage.h"
 #include <message_queue.hpp>
 #include <thread.hpp>
+#include <Adafruit_NeoPixel.h>
 
 using namespace freertos;
 message_queue<uint8_t[]> queue;
@@ -13,7 +14,15 @@ thread SD_thread;
 CAN_Manage CAN;
 SD_Manage SD_manage;
 
+Adafruit_NeoPixel strip(1, 4, NEO_GRB + NEO_KHZ800);
+
+
 void setup() {
+    strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+    strip.show();            // Turn OFF all pixels ASAP
+    strip.setBrightness(255); // Set BRIGHTNESS to about 1/5 (max = 255)
+    strip.setPixelColor(0, strip.Color(255, 0, 0));  
+    strip.show();
     Serial.begin(BAUD_RATE);
     SD_manage.writeFile("/hello.txt", "Hello ");
 
