3d465298d7e84a260d02c8bfb554bf0b51a57354
yagueto <santiago.freire@opendeusto.es>
2025-02-27 10:51:29 +0100

    Update SD_manage.cpp
    Checks queue size. Untested

--- src/SD_manage.cpp
+++ src/SD_manage.cpp
@@ -34,29 +34,35 @@ SD_Manage::SD_Manage() {
 
     // this->deleteFile(SD, "/log.bin");           // Delete file if it exists to start fresh
 
-    SD_thread = freertos::thread::create([](void*){
-        while(true){
-            uint8_t message[CAN_MSG_SIZE];
-            //while(queue.size() > 30) {              // If queue size is greater than 30 we start writing
-                fs::File file;
-                file = SD.open("/log.bin", "ab");
-                while(queue.receive(&message)){      // Writes a binary file with CAN messages   
-                    if (file == NULL) {
-                        Serial.println("Failed to open file for writing");
-                        return;
-                    }
+SD_thread = freertos::thread::create([](void*){
+    while(true){
+        uint8_t message[CAN_MSG_SIZE];
+        
+        // Cast your custom queue to get the underlying FreeRTOS queue handle
+        QueueHandle_t queueHandle = (QueueHandle_t)queue.get_handle(); // Add get_handle() if needed
+        
+        // Check if we have enough messages to process
+        if(uxQueueMessagesWaiting(queueHandle) > 30) {
+            fs::File file;
+            file = SD.open("/log.bin", "ab");
+            while(queue.receive(&message)){
+                if (file) {
                     if(file.write(message, CAN_MSG_SIZE)){
-                        // printf("Message appended\n");
-                    } else printf("Append failed\n");
+                        // Successfully written
+                    } else {
+                        printf("Append failed\n");
+                    }
                     file.flush();
+                } else {
+                    Serial.println("Failed to open file for writing");
+                    break;
                 }
-                file.close();
-            //}
-            delay(100);
+            }
+            file.close();
         }
-    },nullptr,1,4096);
-    SD_thread.start();
-}
+        delay(100);
+    }
+},nullptr,1,4096);
 
 void SD_Manage::listDir(fs::FS &fs, const char * dirname, uint8_t levels){
     Serial.printf("Listing directory: %s\n", dirname);
