82c3ae19ad5dea8afc0b0c0ac14f6bf3ea940b4f
Santiago Freire <santiago.freire@opendeusto.es>
2025-04-10 11:07:56 +0200

    Check queue size before writing

--- src/SD_manage.cpp
+++ src/SD_manage.cpp
@@ -21,6 +21,8 @@ SD_Manage::SD_Manage(xQueueHandle queue)
     if (cardType == CARD_NONE)
     {
         Serial.println("ERROR: No SD card attached");
+        const TickType_t xDelay = 500 / portTICK_PERIOD_MS;
+        while(1) vTaskDelay(xDelay);
         return;
     }
 
@@ -40,27 +42,31 @@ SD_Manage::SD_Manage(xQueueHandle queue)
 
 void SD_Manage::writeQueueToSD()
 {
-    uint8_t message[CAN_MSG_SIZE];
-    fs::File file;
-    file = SD.open("/log.bin", "ab");
     printf("Number of messages in queue: %d\n", uxQueueMessagesWaiting(this->queue));
+    if (uxQueueMessagesWaiting(this->queue) > 20) {
+        uint8_t message[CAN_MSG_SIZE];
+        fs::File file;
+        file = SD.open("/log.bin", "ab");
 
-    while (xQueueReceive(this->queue, &message, 0) == pdTRUE)
-    { // Writes a binary file with CAN messages
-        if (file == NULL)
-        {
-            Serial.println("Failed to open file for writing");
-            return;
+        while (xQueueReceive(this->queue, &message, 0) == pdTRUE)
+        { // Writes a binary file with CAN messages
+            if (file == NULL)
+            {
+                Serial.println("Failed to open file for writing!!!\n");
+                return;
+            }
+            if (file.write(message, CAN_MSG_SIZE))
+            {
+                printf("Message written to SD\n");
+            }
+            else
+                printf("Append failed\n");
+            file.flush();
         }
-        if (file.write(message, CAN_MSG_SIZE))
-        {
-            printf("Message appended\n");
-        }
-        else
-            printf("Append failed\n");
-        file.flush();
+        file.close();
+    } else {
+        printf("less than 20 messages on queue, waiting\n");
     }
-    file.close();
 }
 
 void SD_Manage::listDir(fs::FS &fs, const char *dirname, uint8_t levels)
